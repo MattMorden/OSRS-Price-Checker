5b1d0a52f92385237cb0a6438c924de8fc2899f5
MattMorden <mattymorden@gmail.com>
2017-01-27 17:43:03 -0700

    Base Commit
    OSRS Price checker allows a user to enter a list of item id's and
    retrieve the buying and selling price of the item. This was a quick
    personal project done to learn how to handle dynamic ajax on a web page.

---
+++ RunescapePriceChecker/RunescapePriceChecker/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunescapePriceChecker
+{
+    public class Item
+    {
+        public string overall { get; set; }
+        public string buying { get; set; }
+        public string buyingQuantity { get; set; }
+        public string selling { get; set; }
+        public string sellingQuantity { get; set; }
+
+        public Item()
+        {
+            this.overall = "";
+            this.buying = "";
+            this.buyingQuantity = "";
+            this.selling = "";
+            this.sellingQuantity = "";
+        }
+
+        public Item(string o, string b, string bq, string s, string sq)
+        {
+            this.overall = o;
+            this.buying = b;
+            this.buyingQuantity = bq;
+            this.selling = s;
+            this.sellingQuantity = sq;
+        }
+    }
+}
---
+++ RunescapePriceChecker/RunescapePriceChecker/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RunescapePriceChecker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Lists to hold all the ID's of the items that we will be searching, and the items as responses.
+        List<string> idList = new List<string>();
+        List<Item> itemList = new List<Item>();
+
+        private void searchButton_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = generateOutput(idList);// Fill the label content with results of calling generateOutput
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            string mid = textBox.Text.ToString();
+            idList.Add(mid);
+            listBox1.Items.Add(mid);
+            textBox.Clear();// Clear the textbox so that user can add a new id
+        }
+
+        private string generateOutput(List<string> list)
+        {
+            string output = "";
+            // Loop through each item in the list
+            foreach(var item in list)
+            { 
+            string url = "https://api.rsbuddy.com/grandExchange?a=guidePrice&i=" + item;// Assign id to the end of url
+            var json = new WebClient().DownloadString(url);// Download the JSON string from web page
+            var results =  Newtonsoft.Json.JsonConvert.DeserializeObject<Item>(json);// Convert the json string into an Item object
+  
+            output += "\nItem: " + item + "      Buy: " + results.buying + "      Sell: " + results.selling;
+            }
+            return output;
+        }
+
+
+
+
+
+    }
+}
